bb035b909969d7418d68d4c5cb850ca6fccb7558
eladkeyshawn <elad.keyshawn@gmail.com>
2019-06-29 13:06:55 +0300

    add logs

--- gopro.py
+++ gopro.py
@@ -1,20 +1,35 @@
 # from wifi import Connect 
 import subprocess
 import os
-
+from wireless import Wireless
 from goprocam import GoProCamera, constants
 from gphotos import GPhotoUploader
 
-# Connect('GP24660185')
+wireless = Wireless() 
 
-# gopro = GoProCamera.GoPro()
-# print (gopro)
+def main():
+    print("Switching to GoPro SSID")
+    wireless.connect(ssid='GP24660185', password='epic0298')
+    print("Connected to GoPro SSID.")
 
-# media = gopro.downloadAll()
+    print("Connecting to GoPro...")
+    gopro = GoProCamera.GoPro()
+    print ("GoPro connected.")
 
-# poweroff gopro 
-# switch ssid
+    print("Downloading media...")
+    media = gopro.downloadAll()
+    print("Download complete.")
 
-# upload to google photos
+    print("Powering off GoPro...")
+    gorpo.poweroff()
 
-GPhotoUploader.upload_folder(os.path.abspath('gopro_media'), album='gopro')
+    # upload to google photos
+    print("Switching to Home SSID")
+    wireless.connect(ssid='KS', password='00542274052')
+    print("Connected to Home SSID.")
+
+    print("Starting upload to Google photos...")
+    GPhotoUploader.upload_folder(os.path.abspath('gopro_media'), album='gopro')
+
+if __name__ == "__main__":
+    main()
